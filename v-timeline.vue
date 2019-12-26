58e68e7aa04bc51feeff74ff6d00662bb8feb1a0
Everton de Paula <everton_paula@econeteditora.com.br>
2019-12-26 11:31:42 -0300

    Create component

---
+++ v-timeline.vue
@@ -0,0 +1,35 @@
+<template src="./template.html"></template>
+<style src="./style.css" scoped></style>
+
+<script>
+export default {
+  name: "v-timeline",
+  props: {
+    pointers: null
+  },
+  data() {
+    return {
+      points: this.pointers,
+      pointActive: 1,
+    };
+  },
+  watch: {
+    pointers(value) {
+      this.points = value;
+    }
+  },
+  methods: {
+    clickHandler(obj) {
+      this.move(obj.axis);
+      this.pointActive = obj.id;
+    },
+    move(axis) {
+      window.scroll({
+        top: axis.y || 0,
+        left: axis.x || 0,
+        behavior: "smooth"
+      });
+    }
+  }
+};
+</script>
